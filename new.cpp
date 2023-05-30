b3914756e314a5a82dfc18b49f48d6ba5e77f822
Piyush Varshney <piyushvarshney54@gmail.com>
2023-05-30 16:30:23 +0530

    raor pay

---
+++ new.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long int b;
+    char c;
+    float d;
+    double e;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%c",&c);
+    scanf("%f",&d);
+    scanf("%lf",&e);
+     printf("\n%d ",a);
+    printf("\n%d ",b);
+    printf("\n%c ",c);
+    printf("\n%f ",d);
+    printf("\n%lf ",e);
+    
+    // Complete the code.
+    return 0;
+}
